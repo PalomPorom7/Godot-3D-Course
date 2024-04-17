cfd90fc4afda4dae1c009880dc28be288193cc1d
Thomas Yanuziello <thomas.yanuziello@gmail.com>
2024-04-17 17:12:52 -0400

    1-3 Controls

--- player.gd
+++ player.gd
@@ -1,8 +1,13 @@
 extends Node
 
-var _move_direction : Vector2
+@export var _character : CharacterBody3D
+@export var _camera : Camera3D
+var _input_direction : Vector2
+var _move_direction : Vector3
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta : float):
-	_move_direction = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	print(_move_direction)
+	_input_direction = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	_move_direction = (_camera.basis.x * Vector3(1, 0, 1)).normalized() * _input_direction.x
+	_move_direction += (_camera.basis.z * Vector3(1, 0, 1)).normalized() * _input_direction.y
+	_character.move(_move_direction)
