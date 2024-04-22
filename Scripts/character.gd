a7723056c9361c26ac3a5c37a5735c62ee6e3155
Thomas Yanuziello <thomas.yanuziello@gmail.com>
2024-04-22 16:28:56 -0400

    1-5 Walking

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -1,8 +1,14 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+
+@export var _walking_speed : float = 1
+@export var _acceleration : float = 2
+@export var _deceleration : float = 4
+var _xz_velocity : Vector3
+
+@onready var _animation : AnimationTree = $AnimationTree
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -12,6 +18,8 @@ func move(direction : Vector3):
 	_direction = direction
 
 func _physics_process(delta):
+	# Copy the character's x and z velocity to isolate from y.
+	_xz_velocity = Vector3(velocity.x, 0, velocity.z)
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -20,15 +28,16 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	#var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	# Apply movement input to the xz velocity
 	if _direction:
-		velocity.x = _direction.x * SPEED
-		velocity.z = _direction.z * SPEED
+		_xz_velocity = _xz_velocity.move_toward(_direction * _walking_speed, _acceleration * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		_xz_velocity = _xz_velocity.move_toward(Vector3.ZERO, _deceleration * delta)
+	
+	_animation.set("parameters/Locomotion/blend_position", _xz_velocity.length() / _walking_speed)
+
+	# Apply adjusted xz velocity to the character
+	velocity.x = _xz_velocity.x
+	velocity.z = _xz_velocity.z
 
 	move_and_slide()
