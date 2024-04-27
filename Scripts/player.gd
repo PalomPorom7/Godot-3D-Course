17fb2f5d89436950f64c52a04f5657d1bf613a38
Thomas Yanuziello <thomas.yanuziello@gmail.com>
2024-04-27 19:20:23 -0400

    1-9 Camera

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export var _character : CharacterBody3D
+@export var _spring_arm : SpringArm3D
 @export var _camera : Camera3D
 var _input_direction : Vector2
 var _move_direction : Vector3
@@ -11,10 +12,13 @@ func _input(event : InputEvent):
 	elif event.is_action_released("run"):
 		_character.walk()
 	elif event.is_action_pressed("jump"):
-		_character.jump()
+		_character.start_jump()
+	elif event.is_action_released("jump"):
+		_character.complete_jump()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta : float):
+	_spring_arm.look(Input.get_vector("look_left", "look_right", "look_up", "look_down"))
 	_input_direction = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	_move_direction = (_camera.basis.x * Vector3(1, 0, 1)).normalized() * _input_direction.x
 	_move_direction += (_camera.basis.z * Vector3(1, 0, 1)).normalized() * _input_direction.y
